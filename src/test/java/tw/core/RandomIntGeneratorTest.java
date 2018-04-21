db09a4e7064b4d2ca9399914a11cd56e49b81a7c
colining <18810782954@163.com>
2018-04-21 14:26:08 +0800

    fix RandomIntGeneratorTest

--- src/test/java/tw/core/RandomIntGeneratorTest.java
+++ src/test/java/tw/core/RandomIntGeneratorTest.java
@@ -1,9 +1,24 @@
 package tw.core;
 
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import tw.core.generator.RandomIntGenerator;
+
 /**
  * 在RandomIntGeneratorTest文件中完成RandomIntGenerator中对应的单元测试
  */
 public class RandomIntGeneratorTest {
+    RandomIntGenerator randomIntGenerator = null;
+    @Before
+    public void setup() {
+        randomIntGenerator = new RandomIntGenerator();
+    }
 
+    @Test
+    public void generateNums_numberOfNeed() {
+        Assert.assertEquals(7, randomIntGenerator.generateNums(9, 4).length());
+        Assert.assertEquals(17, randomIntGenerator.generateNums(9, 9).length());
+    }
 }
